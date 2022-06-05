0f7cb0d7715a7df7ed8f52dd60ddfa1915384ded
arindra97 <ar28297@gmail.com>
2022-06-05 11:37:46 +0700

    fix: task challenge callback

--- index.js
+++ index.js
@@ -6,15 +6,27 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (name, cbSort) =>{
+  let result = [];
+  for (let i = 0; i < name.length; i++) {
+    result.push(`${i+1}. ${cbSort(name)[i]}`);
+  }
+  return result;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (name) => {
+  const ascName = name.sort(); 
+  return ascName;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (name)=> {
+  const descName = name.sort().reverse();
+  return descName;
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
